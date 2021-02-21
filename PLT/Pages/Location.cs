3ad62f5e381ee5dd8e21c82f99667ea8421f35a1
EricAwesomeness <79414852+EricAwesomeness@users.noreply.github.com>
2021-02-21 13:26:00 -0500

    Initial Upload
    Initial Upload

---
+++ PLT/Pages/Location.cs
@@ -0,0 +1,55 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLT.Pages
+{
+    public class Location
+    {
+
+
+        private string locationName;
+        public string LocationName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(locationName))
+                {
+                    return "N/A";
+                }
+                return locationName;
+            }
+            set
+            {
+                locationName = value;
+            }
+        }
+
+
+
+        private ObservableCollection<Department> departments;
+        public ObservableCollection<Department> Departments
+        {
+            get
+            {
+                return departments;
+            }
+            set
+            {
+                departments = value;
+            }
+        }
+
+
+
+        public Location(string locationName)
+        {
+            LocationName = locationName;
+            departments = new ObservableCollection<Department>(){};
+        }
+    }
+}
---
+++ PLT/Pages/Printer.cs
@@ -0,0 +1,109 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLT.Pages
+{
+    public class Printer 
+    {
+
+        private string warrantycode = string.Empty;
+        private string model = string.Empty;
+        private string location = string.Empty;
+        private string ip = string.Empty;
+        private string department = string.Empty;
+
+
+        public string WarrantyCode
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(warrantycode))
+                {
+                    return "N/A";
+                }
+                return warrantycode;
+            }
+            set
+            {
+                warrantycode = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(model))
+                {
+                    return "N/A";
+                }
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+        public string Location
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(location))
+                {
+                    return "N/A";
+                }
+                return location;
+            }
+            set
+            {
+                location = value;
+            }
+        }
+        public string Ip
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ip))
+                {
+                    return "N/A";
+                }
+                return ip;
+            }
+            set
+            {
+                ip = value;
+            }
+        }
+        public string Department
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(department))
+                {
+                    return "N/A";
+                }
+                return department;
+            }
+            set
+            {
+                department = value;
+            }
+        }
+
+
+
+        public Printer(string warrantycode, string model, string location, string ip, string department)
+        {
+
+            WarrantyCode = warrantycode;
+            Model = model;
+            Location = location;
+            Ip = ip;
+            Department = department;
+           
+        }
+    }
+}
