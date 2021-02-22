750e74ff71d2a13ffd0f0b6018d07325e1807d21
EricAwesomeness <79414852+EricAwesomeness@users.noreply.github.com>
2021-02-22 15:25:32 -0500

    ViewInfrastructure Folder Added
    "ViewInfrastructure" Folder Added Containing a class that changes selected tree view item behavior to be able to 2 way bind.

---
+++ PLT/ViewInfrastructure/SelectedTreeViewItemBehavior.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PLT.ViewInfrastructure
+{
+    /// <summary>
+    /// Behavior that enables twoway binding to the selected item property of a WPF treeview
+    /// </summary>
+    /// <remarks>
+    /// requires the xmlns:i="http://schemas.microsoft.com/expression/2010/interactivity" namespace
+    /// </remarks>
+    /// <example>
+    /// Here is how to use the behavior in XAML:
+    /// <code>
+    /// <i:Interaction.Behaviors>
+    ///     <local:SelectedTreeViewItemBehavior SelectedItem = "{Binding SomeViewModel.SomeProperty}" />
+    /// </ i:Interaction.Behaviors>
+    /// </code>
+    /// </example>
+    // source: https://stackoverflow.com/questions/11065995/binding-selecteditem-in-a-hierarchicaldatatemplate-applied-wpf-treeview/18700099#18700099
+    public sealed class SelectedTreeViewItemBehavior : Behavior<TreeView>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.SelectedItemChanged += OnTreeViewSelectedItemChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            if (AssociatedObject is null)
+            {
+                return;
+            }
+
+            AssociatedObject.SelectedItemChanged -= OnTreeViewSelectedItemChanged;
+        }
+
+        private void OnTreeViewSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            SetCurrentValue(SelectedItemProperty, e.NewValue);
+        }
+
+        public object SelectedItem
+        {
+            get { return GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        /// <summary>Identifies the <see cref="SelectedItem"/> dependency property.</summary>
+        public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register(
+            nameof(SelectedItem),
+            typeof(object),
+            typeof(SelectedTreeViewItemBehavior),
+             new FrameworkPropertyMetadata(default, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+    }
+}
